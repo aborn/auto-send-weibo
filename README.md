cec0bb3dda279bbf5f0b34491648d5b6dfc025ab
aborn <loveaborn@foxmail.com>
2013-10-02 21:40:24 +0800

    release v0.1

--- README.md
+++ README.md
@@ -1,29 +1,36 @@
 auto-send-weibo
 ===============
 
-# INTRODUCTION
-	This project is used to automatically send weibo in Linux platform, which includes Ubuntu, Raspberry Pi etc.
-# NOTE
-### Before you use this code, please confirm:
-1. You must have an weibo account. Please apple it form www.weibo.com if you don't.
-2. You should create an application in open.weibo.com and get the App Key
+## INTRODUCTION
 
-### In this README.md, ${ABSPATH} means this file path
+This project is used to automatically send weibo in Linux 
+platform, which includes Ubuntu, Raspberry Pi etc.
 
-### This project use shell(bash) scripts.
+## NOTE
+#### Before you use this code, please confirm:
+* You must have an weibo account. Please apple it form www.weibo.com if you don't.
 
-# USAGE
+#### In this README.md, ${ABSPATH} means this file path
+
+#### This project use shell(bash) scripts.
+
+## USAGE
 1. Edit file account.cfg to configure your account infomation
 2. Edit weibo content in file ${ABSPATH}/weibo/weibo.cont
-3. All pictures/images are stored in ${ABSPATH}/weibo/
-4. Edit picture list in image.list
+3. All pictures/images are stored in ${ABSPATH}/images/
+4. Edit picture list in images.list
 5. Running: ${ABSPATH}/auto-send.sh or ./auto-send.sh
 
-# EXAMPLE
-	./auto-send.sh
+## EXAMPLE
+	./auto-send.sh -h    # get help msg
+    ./auto-send.sh -c  "My first test microblog"
+    ./auto-send.sh -f 
+    ./auto-send.sh -p ./images/pi.png -f
 
-# AUTHOR
+
+## AUTHOR
     Aborn Jiang (loveaborn@foxmail.com)
 
-# VERSION
+## VERSION
     V0.01  2013-10-02
+    v0.1   2013-10-02
--- auto-send.sh
+++ auto-send.sh
@@ -1,24 +1,120 @@
 #!/bin/bash
-##################################
-# auto send weibo in console
-##################################
+###################################################################
+# NAME
+#     auto-send.sh automatically send microblog(weibo) in linux
+# USAGE
+#     ./auto-send.sh [-f|-c  |"content"] [ -p /path/to/pic.png]
+# OPTIONS
+#     -h       this help manual.
+#     -f       send file './weibo/weibo.cont' content
+#     -c       [contents], short content text information
+#     -p       image path default:./images/weibo.png
+# EXAMPLE
+#      ./auto-send.sh -c "My first test microblog" 
+#      ./auto-send.sh -f 
+#      ./auto-send.sh -p ./images/pi.png -f
+# NOTE
+#      This version only support single picture
+# AUTHOR
+#      Aborn Jiang (loveaborn@foxmail.com)
+# VERSION
+#      v0.1  2013-10-02
+###################################################################
 
+source help_info.sh
 ABSPATH=$(cd $(dirname $0); pwd);
-#echo "Working dir:${ABSPATH}"
 CFGFILE="account.cfg"
-CONTFILE="${ABSPATH}/weibo/weibo.cont"
 updateURL="https://api.weibo.com/2/statuses/update.json"
+uploadURL="https://upload.api.weibo.com/2/statuses/upload.json"
 
-if [ -f ${CFGFILE} ];then
-	chmod 600 ${CFGFILE}
-	source ${CFGFILE}
-else
-	echo "no configure ${CFGFILE}."
-	exit 1
-fi
+[ $# -eq 0 ] && help_info && exit 1
+while getopts hfc:p:l: opt
+do
+	case "$opt" in
+		h) help_info ;exit 0;;                    # this help_info function in ./scripts/functions.sh
+		f) TXTTYPE="file";;
+		c) TXTTYPE="txtmsg"; CONTENT=${OPTARG};;
+		p) IMGTAG="true";IMGPATH=${OPTARG};;
+		l) IMGLIST=${OPTARG};;
+		*) echo "Unknown option:$opt";help_info;exit 1;; 
+	esac
+done
 
-if [ -s ${CONTFILE} ];then
-	curl -u ${USER}:${PASSWORD} -d "source=${APPKEY}" -d "status=$(cat ${CONTFILE})" ${updateURL} > /dev/null
-else
-	echo "no more content to send!"
-fi
+[ -z ${CONTFILE} ] && CONTFILE="${ABSPATH}/weibo/weibo.cont"
+[ -z ${IMGPATH} ]  && IMGPATH="${ABSPATH}/images/weibo.png"
+[ -z ${IMGLIST} ]  && IMGLIST="images.list"
+
+
+function configure()
+{
+	if [ -f ${CFGFILE} ];then
+		chmod 600 ${CFGFILE}
+		source ${CFGFILE}
+	else
+		echo "no configure ${CFGFILE}."
+		exit 1
+	fi
+}
+
+function deal-text-cont()
+{
+	if [ ! -z ${TXTTYPE} ];then
+		[ ${TXTTYPE} == "file" ] && CONTENT=$(cat ${CONTFILE})
+	fi
+}
+
+function deal-with-image()
+{
+	IMGPATH=$1
+	IMGLISTCONT=""
+	[ ! -s ${IMGLIST} ] && echo "image list doesn't exist!" && exit 1
+	lists=$(cat ${IMGLIST} |egrep -v "^#")
+	for iterm in ${lists}
+	do
+		IMGLISTCONT=" ${IMGLISTCONT} -F \"pic=@./images/${iterm}\" "
+	done
+	#echo ${IMGLISTCONT}
+	#IMGCONT=" \"pic=@${IMGPATH}\"  "
+
+	len=$(echo ${CONTENT} |wc -w)
+	if  [ ${len} -eq 0 ];then
+		CONTENT="See this picture. 见图."
+	fi
+}
+
+function send-text-only()
+{
+	length=$(echo ${CONTENT} |wc -w)
+	if [ ${length} -ne 0 ];then 
+		echo "send microblog only text"
+		curl -u ${USER}:${PASSWORD} -d "source=${APPKEY}" -d "status=$(echo ${CONTENT})" ${updateURL} > /dev/null
+	else
+		echo "no more content to send!"
+	fi
+}
+
+function send-text-pic()
+{
+	if [ -f ${IMGPATH} ];then 
+		deal-with-image ${IMGPATH}
+		curl -u ${USER}:${PASSWORD} -F "source=${APPKEY}" -F "status=$(echo ${CONTENT})"  -F "pic=@${IMGPATH}" ${uploadURL} > /dev/null
+		echo "send microblog with picture"
+	else
+		echo "the image: ${IMGPATH} doesn't exist! Send text only."
+		send-text-only
+	fi
+}
+
+function main()
+{
+	configure
+	deal-text-cont
+	if [ ! -z ${IMGTAG} ];then
+		send-text-pic
+	else
+		send-text-only
+	fi
+	exit 0
+}
+
+main
